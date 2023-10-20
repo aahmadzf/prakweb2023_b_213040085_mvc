2bbfdc703e10c9c6b2f59b469445dc55b67dd156
aahmadzf <aahmadzf0@gmail.com>
2023-10-20 23:18:10 +0700

    MVC - Update Data (+ajax)

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -7,7 +7,7 @@
 
     <div class="row">
         <div class="col-6">
-            <button type="button" class="btn btn-primary mb-4" data-bs-toggle="modal" data-bs-target="#formModal">
+            <button type="button" class="btn btn-primary mb-4 tombolTambahData"  data-bs-toggle="modal" data-bs-target="#formModal">
                 Tambah Mahasiswa
             </button>
 
@@ -17,6 +17,9 @@
                     <?php foreach ($data['mhs'] as $mhs): ?>
                     <li class="list-group-item "><?=$mhs['nama'];?>
                     <a href=" <?=BASEURL;?>/mahasiswa/hapus/<?=$mhs['id'];?>" class="badge text-bg-danger float-end me-2" onclick= "return confirm('Yakin ingin menghapus?')" >Hapus</a>
+
+                    <a href=" <?=BASEURL;?>/mahasiswa/ubah/<?=$mhs['id'];?>" class="badge text-bg-success float-end me-2 tampilModalUbah" data-bs-toggle="modal" data-bs-target="#formModal" data-id="<?=$mhs['id'];?>">Ubah</a>
+
                     <a href=" <?=BASEURL;?>/mahasiswa/detail/<?=$mhs['id'];?>" class="badge text-bg-primary float-end me-2">Detail</a>
                     </li>
                     <?php endforeach;?>
@@ -28,15 +31,17 @@
 </div>
 
 <!-- MODAL -->
-<div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="judulModal" aria-hidden="true">
+<div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="formModal" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="judulModal">Tambah Data Mahasiswa</h1>
+        <h1 class="modal-title fs-5" id="formModalLabel">Tambah Data Mahasiswa</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
+
         <form action="<?=BASEURL?>/mahasiswa/tambah" method="post">
+        <input type="hidden" name="id" id="id">
             <div class="mb-3">
                 <label for="nama" class="form-label">Nama</label>
                 <input type="text" class="form-control" id="nama" name="nama">
@@ -65,6 +70,7 @@
             </div>
 
       </div>
+
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
         <button type="submit" class="btn btn-primary">Tambah Data</button>
